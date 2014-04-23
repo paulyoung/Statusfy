5daabc5341c414735cd801d9d7f17cb6d04f7e06
Paul Young <paulyoungonline@gmail.com>
2014-04-23 13:21:16 -0400

    Add player state to status bar. * Fixes #3. * Update logic around running state of Spotify. * Update text for when Spotify is not running.

--- Statusfy/SFYAppDelegate.m
+++ Statusfy/SFYAppDelegate.m
@@ -33,25 +33,40 @@
 
 - (void)setStatusItemTitle
 {
+    NSString *titleText = NSLocalizedString(@"Statusfy (Spotify not running)", nil);
+    
     NSAppleScript *runningScript = [[NSAppleScript alloc] initWithSource:@"get running of application \"Spotify\""];
     NSDictionary *runningError;
     BOOL running = [[runningScript executeAndReturnError:&runningError] booleanValue];
     
-    NSString *titleText = nil;
-    
     if (running) {
-        NSAppleScript *trackNameScript = [[NSAppleScript alloc] initWithSource:@"tell application \"Spotify\" to get name of current track"];
+        NSAppleScript *trackNameScript = [[NSAppleScript alloc] initWithSource:@"if application \"Spotify\" is running then tell application \"Spotify\" to get name of current track"];
         NSDictionary *trackNameError;
         NSString *trackName = [[trackNameScript executeAndReturnError:&trackNameError] stringValue];
         
-        NSAppleScript *artistNameScript = [[NSAppleScript alloc] initWithSource:@"tell application \"Spotify\" to get artist of current track"];
+        NSAppleScript *artistNameScript = [[NSAppleScript alloc] initWithSource:@"if application \"Spotify\" is running then tell application \"Spotify\" to get artist of current track"];
         NSDictionary *artistNameError;
         NSString *artistName = [[artistNameScript executeAndReturnError:&artistNameError] stringValue];
         
-        titleText = [NSString stringWithFormat:@"%@ - %@", trackName, artistName];
-    }
-    else {
-        titleText = NSLocalizedString(@"Statusfy", nil);
+        NSAppleScript *playerStateScript = [[NSAppleScript alloc] initWithSource:@"if application \"Spotify\" is running then tell application \"Spotify\" to get player state"];
+        NSDictionary *playerStateError;
+        NSString *playerStateConstant = [[playerStateScript executeAndReturnError:&playerStateError] stringValue];
+        
+        NSString *playerState = nil;
+        
+        if ([playerStateConstant isEqualToString:@"kPSP"]) {
+            playerState = NSLocalizedString(@"Playing", nil);
+        }
+        else if ([playerStateConstant isEqualToString:@"kPSp"]) {
+            playerState = NSLocalizedString(@"Paused", nil);
+        }
+        else {
+            playerState = NSLocalizedString(@"Stopped", nil);
+        }
+        
+        if (trackName && artistName) {
+            titleText = [NSString stringWithFormat:@"%@ - %@ (%@)", trackName, artistName, playerState];
+        }
     }
     
     self.statusItem.title = titleText;
